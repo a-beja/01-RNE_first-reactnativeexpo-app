d0b90fa01767932368c62528e6fb4b809b6c9f0f
Abril <abrilm.dev@gmail.com>
2026-06-12 20:50:58 -0600

    Implement counter functionality with a floating button in App component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={ styles.textHuge }>{ count }</Text>
+
+      <Pressable
+        style={ styles.floatingButton }
+        onPress={ ()=> setCount(count + 1) }
+        onLongPress={ () => setCount(0) }
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}> +1 </Text>
+      </Pressable>
+
+      {/* <TouchableOpacity>
+        <Text> +1 </Text>
+      </TouchableOpacity> */}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
+  },
+
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+  }
 });
